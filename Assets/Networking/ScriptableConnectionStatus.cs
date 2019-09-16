a464383144d359036b4040e8ec49ebc56da4ae32
Olivier Perrault <olivier.perrault@usherbrooke.ca>
2019-09-16 09:34:01 -0400

    save progress

---
+++ Assets/Networking/ScriptableConnectionStatus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using UdeS.Promoscience.ScriptableObjects;
+using UdeS.Promoscience.Utils;
+using UdeS.Promoscience.Network;
+
+namespace UdeS.Promoscience.Network
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "Data/Connection Status", order = 1)]
+    public class ScriptableConnectionStatus : ScriptableObject
+    {
+        public enum ConnectionStatus
+        {
+            Pairing,
+            PairingSuccess,
+            PairingFail,
+            ConnectingToPair,
+            ConnectingToServer,
+            Connected,
+            Ready,
+        }
+
+        private ConnectionStatus value = ConnectionStatus.Pairing;
+
+        public void Awake()
+        {
+            value = ConnectionStatus.Pairing;        
+        }
+
+        public ConnectionStatus Value
+        {
+            get
+            {
+                return value;
+            }
+        }
+    }
+}
+
