e4f1c943e60c358efa07a513110c72a0009d22f5
Olivier Perrault <olivier.perrault@usherbrooke.ca>
2019-11-23 15:32:00 -0500

    added instruction menu

---
+++ Assets/Menu/InstructionManager.cs
@@ -0,0 +1,42 @@
+﻿using Cirrus.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UdeS.Promoscience.Menu
+{
+    public class InstructionManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] pages;
+
+        private int currentPageIndex = 0;
+
+        [SerializeField]
+        private UnityEngine.UI.Button previousButton;
+
+
+        [SerializeField]
+        private UnityEngine.UI.Button nextButton;
+
+        public void Awake()
+        {
+            previousButton.onClick.AddListener(() =>
+            {
+                pages[currentPageIndex].SetActive(false);
+                currentPageIndex--;
+                currentPageIndex = currentPageIndex.Mod(pages.Length);
+                pages[currentPageIndex].SetActive(true);
+            });
+
+            nextButton.onClick.AddListener(() =>
+            {
+                pages[currentPageIndex].SetActive(false);
+                currentPageIndex++;
+                currentPageIndex = currentPageIndex.Mod(pages.Length);
+                pages[currentPageIndex].SetActive(true);
+            });
+        }
+
+    }
+}
