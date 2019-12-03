3c871e89f329b2e58e8b9449c06363814c942172
Olivier Perrault <olivier.perrault@usherbrooke.ca>
2019-12-03 02:29:39 -0500

    save

--- Assets/Networking/Fix/NetworkDiscovery.cs
+++ Assets/Networking/Fix/NetworkDiscovery.cs
@@ -73,7 +73,6 @@ public class NetworkDiscoveryFix : NetworkDiscovery
             NetworkTransport.Shutdown();
             NetworkTransport.Init();
 
-
             Initialize();
             StartAsClient();
 
