eb3966e3a4032b0347fbe5cbf50a2a3f0b5c8481
Olivier Perrault <olivier.perrault@usherbrooke.ca>
2019-12-12 09:28:57 -0500

    save

---
+++ Assets/Replays/Level/UI/SelectedAlgorithmDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UdeS.Promoscience.Replays.UI
+{
+
+    public class SelectedAlgorithmDisplay : MonoBehaviour
+    {
+        [SerializeField]
+        private AlgorithmSelectionAsset asset;
+
+        [SerializeField]
+        private UnityEngine.UI.Text steps;
+
+        public void Awake()
+        {
+            
+        }
+
+
+    }
+}
